c2bdbe9ffdb2bfa45edf3ff3df5b1c7687ab9832
Usama Safdar Khan <usama_safdarkhan@outlook.com>
2018-01-15 14:40:33 +0500

    first commit

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  database: process.env.DATABASE || 'mongodb://root:usama123@ds235807.mlab.com:35807/bzu_multan',
+  port: process.env.PORT || 3000,
+  secret: process.env.SECRET || 'usama123',
+
+}
